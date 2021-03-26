6a6aa18718262226378035bc235891da6a7f0d45
Ednilson Miura <emiura@suse.com>
2021-03-26 11:31:25 -0300

    Fix parser for single commands inside script_run

--- parser.sh
+++ parser.sh
@@ -11,16 +11,10 @@ array_size=${#cmd_array[@]}
 
 # discount one, so shell does not return empty position
 let array_size--
-for i in $(seq 0 $array_size); do
-    echo "script_run ============================="
-    echo ${cmd_array[$i]}
-done
 
 # get only cmds, not parameters
-echo "commands ================================"
 for j in $(seq 0 $array_size); do
     if [[ ${cmd_array[$j]} =~ ";" ]]; then
-        echo "tem ;"
         temp=$(echo ${cmd_array[@]} | tr ";" "\n" | sed 's/^ //g' | cut -d " " -f 1 | grep [a-zA-Z])
         for entry in $temp; do
             temp_list=("${temp_list[@]}" "$entry")
@@ -29,26 +23,14 @@ for j in $(seq 0 $array_size); do
         temp=$(echo ${cmd_array[j]} | sed 's/^ //g' | cut -d " " -f 1 | grep [a-zA-Z])
         temp_list=("${temp_list[@]}" "$temp")
     fi
-    #no_dups=$(echo $temp | xargs -n1 | sort -u | xargs)
 done
-    echo $temp_list
-exit
-#for j in $(seq 0 $array_size); do
-#    if [[ ${cmd_array[$j]} =~ ";" ]]; then
-#        echo "BLE"
-#        echo ${cmd_array[@]} | tr ";" "\n" | sed 's/^ //g' | cut -d " " -f 1 | grep [a-zA-Z]
-#    else
-#        echo "BLI"
-#        echo ${cmd_array[@]} | sed 's/^ //g' | cut -d " " -f 1 | grep [a-zA-Z]
-#    fi
-#done
 
-exit
-echo LALA
-echo $cmds
-dups=$(echo $cmds | xargs -n1 | sort -u | xargs)
-echo "individual ======================================================================="
-echo $dups
+# need more smart way to do this
+for v in ${temp_list[@]}; do
+    if [[ ! ${final_list[@]} =~ "$v" ]]; then
+        final_list=("${final_list[@]}" "$v")
+    fi
+done
 
 # refresh repo
 zypper ref
@@ -58,9 +40,8 @@ if ! test -x /usr/bin/scout; then
     zypper --non-interactive install -y scout
 fi
 
-for cmd in $dups; do
+for cmd in ${final_list[@]}; do
     exe=$(scout bin $cmd | cut -d "|" -f 2 | grep -v package | grep [a-zA-Z] | tr -d " " | uniq)
     exe_list=("${exe_list[@]}" "$cmd:$exe")
 done
-echo "list ========================================================================"
 echo ${exe_list[@]}
